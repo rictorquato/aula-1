99523d826b70b885494a6524e23c11cbb299020c
rictorquato <159740164+rictorquato@users.noreply.github.com>
2026-03-17 01:16:18 +0000

    Criação da calculadora de imc

--- imc.c
+++ imc.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
 
+int main (){
+
+    float peso;
+    peso = 75;
+
+    float altura;
+    altura = 1.79;
+
+    float alturaXaltura;
+    alturaXaltura = altura * altura;
+
+    float imc;
+    imc = peso / alturaXaltura;
+    
+    printf ("o imc calculado é: %lf.", imc);
+
+    return 0;
+}
